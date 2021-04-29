44de0fd2e717108c120e82f50f0ed61306fd97f5
Ivan <reinkarnaci9@gmail.com>
2021-04-29 22:22:51 +0300

    сессион конфиг закоменчен

--- eshop-ui/src/main/java/ru/geekbrains/SessionConfig.java
+++ eshop-ui/src/main/java/ru/geekbrains/SessionConfig.java
@@ -1,13 +1,13 @@
-package ru.geekbrains;
-
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.RedisSerializer;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
-
+//package ru.geekbrains;
+//
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Profile;
+//import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+//import org.springframework.data.redis.serializer.RedisSerializer;
+//import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+//
 //@Profile("!dev")
 //@Configuration
 //@EnableRedisHttpSession
