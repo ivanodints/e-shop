c5f4979b5fc64494e0847745e7852ba977f0aeb7
Ivan <reinkarnaci9@gmail.com>
2021-05-14 21:34:24 +0300

    Rename Encoder

--- eshop-ui/src/main/java/ru/geekbrains/EshopUiApplication.java
+++ eshop-ui/src/main/java/ru/geekbrains/EshopUiApplication.java
@@ -4,14 +4,15 @@ package ru.geekbrains;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class EshopUiApplication {
 
-
+    @Profile("!dev")
     @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
+    public BCryptPasswordEncoder passEncoder() {
         return new BCryptPasswordEncoder();
     }
 
